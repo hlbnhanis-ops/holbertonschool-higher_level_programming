d3c02505ab2188822d7ddbb78faa2ca39a0982ce
hlbnhanis-ops <hlbn.hanis@gmail.com>
2025-11-18 13:56:06 +0100

    Update 9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = abs(number) % 10  
-    print(last_digit, end="")      
+    last_digit = abs(number) % 10    
     return last_digit               
