71bdb38cd8ae0b1fcdbe85f1dc88236bc3defd1f
hlbnhanis-ops <hlbn.hanis@gmail.com>
2025-11-19 10:28:37 +0100

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(8):
-    for j in range(i+1, 10):
-        print(str(i) + str(j) + ", ", end="")
-print(str(89))
+
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i != 8 or j != 9:
+            print('{:d}{:d}, '.format(i, j), end='')
+print('{:d}{:d}'.format(8, 9))
