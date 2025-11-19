13cd7adf32664860d68f9053cc2ea53b2bcc4e40
hlbnhanis-ops <hlbn.hanis@gmail.com>
2025-11-19 10:15:01 +0100

    Update 3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in [range(ord('a'),ord('e')), range(ord('f'),ord('q')), range(ord('r'),ord('z'))]:
-    print('{}'.format(chr(ord('a') + i)), end='')
+    print('{}'.format(chr(i)), end='')
