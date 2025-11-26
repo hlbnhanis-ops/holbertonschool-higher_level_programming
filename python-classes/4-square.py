5f1ebc74b5d20f257080ada88046e8f102e1a8c3
hlbnhanis-ops <hlbn.hanis@gmail.com>
2025-11-26 15:10:55 +0100

    Update 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -2,14 +2,23 @@
 
 class Square:
     def __init__(self, size=0):
+        """
+        x
+        """
         self.size = size  # uses the setter for validation
 
     @property
     def size(self):
+        """
+        x
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        x
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -17,5 +26,8 @@ class Square:
         self.__size = value
 
     def area(self):
+        """
+        x
+        """
         return self.__size ** 2
     
