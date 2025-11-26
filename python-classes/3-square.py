5da4095ab0799da46c81bd78c229a1578e81eb0a
hlbnhanis-ops <hlbn.hanis@gmail.com>
2025-11-26 15:09:28 +0100

    Update 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,12 +2,21 @@
 
 class Square:
     def __init__(self, size=0):
+        """
+        x
+        """
         if not isinstance(size, int):
+            """
+            x
+            """
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
+        """
+        x
+        """
         return self.__size ** 2
     
